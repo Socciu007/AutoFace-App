fb24d43bd71da7f34dc55bf14a59f6c4cacbba23
Nguyen Duc An <annd.oneadx@gmail.com>
2023-12-01 17:56:13 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import { ReactNotifications } from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard/Dashboard";
+import ScriptManager from "./pages/Script-Manager";
 
 function App() {
   return (
     <>
       <ReactNotifications />
       <Routes>
-        <Route path="/" element={<Dashboard></Dashboard>} />
+        <Route path="/" element={<ScriptManager></ScriptManager>} />
       </Routes>
     </>
   );
