7476c491a5bdcf140972b5cb552854097f097fba
Nguyen Duc An <annd.oneadx@gmail.com>
2023-12-05 18:05:37 +0700

    create script page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { ReactNotifications } from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
 import { Route, Routes } from "react-router-dom";
 import ScriptManager from "./pages/Script-Manager";
-
+import CreateScript from "./pages/Create-Script";
 function App() {
   return (
     <>
       <ReactNotifications />
       <Routes>
         <Route path="/" element={<ScriptManager></ScriptManager>} />
+        <Route path="/create" element={<CreateScript></CreateScript>} />
       </Routes>
     </>
   );
