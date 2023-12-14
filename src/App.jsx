ae6b0c993f668b1b00d84fb24e3ff154e0dc1a7e
Tiền Vũ <tienvm.oneadx@gmail.com>
2023-12-14 09:37:40 +0700

    tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-// import { useState } from "react";
+import { useState } from 'react';
 import { ReactNotifications } from 'react-notifications-component';
 import 'react-notifications-component/dist/theme.css';
 import { Route, Routes } from 'react-router-dom';
-import ProfilesPage from './pages/ProfilesPage/ProfilesPage';
-import SettingsPage from './pages/SettingsPage/SettingsPage';
-import ProxySettingsPage from './pages/ProxySettingsPage/ProxySettingsPage';
-import CreateScript from './components/CreateScriptComponent/CreateScript';
+import CreateScript from './pages/Create-Script/index.jsx';
 import ScriptManager from './pages/Script-Manager/index.jsx';
+import SettingsPage from './pages/SettingsPage/SettingsPage.jsx';
+import ProxySettingsPage from './pages/ProxySettingsPage/ProxySettingsPage.jsx';
+import ProfilesPage from './pages/ProfilesPage/ProfilesPage.jsx';
 function App() {
   return (
     <>
       <ReactNotifications />
       <Routes>
         <Route path="/create" element={<CreateScript></CreateScript>} />
-        <Route path="/scripManager" element={<ScriptManager></ScriptManager>} />
-        <Route path="/" element={<ProfilesPage></ProfilesPage>} />
+        <Route path="/scripManager" element={<ScriptManager></ScriptManager>} />{' '}
         <Route path="/settings" element={<SettingsPage></SettingsPage>} />
         <Route path="/settings-proxy" element={<ProxySettingsPage></ProxySettingsPage>} />
+        <Route path="/" element={<ProfilesPage></ProfilesPage>} />
       </Routes>
     </>
   );
